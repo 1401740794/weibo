b00de52d9c8cfe664a74981198bd08da89a6bd02
cancan <1401740794@qq.com>
2019-05-10 07:54:35 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +10,10 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));//用户对象 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法
     }
 }
